94ffc0c01c706fa3f6a72694c32a4bed9f3273ca
Shlee <github@shl.ee>
2025-02-13 13:52:37 +1030

    Update FileByteSinkSource.java

--- src/main/java/com/jortage/poolmgr/util/FileByteSinkSource.java
+++ src/main/java/com/jortage/poolmgr/util/FileByteSinkSource.java
@@ -8,29 +8,49 @@ import com.google.common.io.Files;
 
 public class FileByteSinkSource implements ByteSinkSource {
 
-	private final File file;
-	private final boolean deleteOnClose;
+    private final File file;
+    private final boolean deleteOnClose;
 
-	public FileByteSinkSource(File file, boolean deleteOnClose) {
-		this.file = file;
-		this.deleteOnClose = deleteOnClose;
-	}
-	
-	@Override
-	public ByteSink getSink() {
-		return Files.asByteSink(file);
-	}
-	
-	@Override
-	public ByteSource getSource() {
-		return Files.asByteSource(file);
-	}
-	
-	@Override
-	public void close() {
-		if (deleteOnClose) {
-			file.delete();
-		}
-	}
-	
+    /**
+     * Constructs a new FileByteSinkSource.
+     *
+     * @param file The file used for byte storage.
+     * @param deleteOnClose If true, deletes the file when close() is called.
+     */
+    public FileByteSinkSource(File file, boolean deleteOnClose) {
+        this.file = file;
+        this.deleteOnClose = deleteOnClose;
+    }
+
+    /**
+     * Returns a ByteSink for writing to the file.
+     *
+     * @return a ByteSink for the file.
+     */
+    @Override
+    public ByteSink getSink() {
+        return Files.asByteSink(file);
+    }
+
+    /**
+     * Returns a ByteSource for reading from the file.
+     *
+     * @return a ByteSource for the file.
+     */
+    @Override
+    public ByteSource getSource() {
+        return Files.asByteSource(file);
+    }
+
+    /**
+     * Closes this resource, deleting the file if {@code deleteOnClose} is true.
+     */
+    @Override
+    public void close() {
+        if (deleteOnClose && file.exists()) {
+            if (!file.delete()) {
+                System.err.println("Warning: Failed to delete file " + file.getAbsolutePath());
+            }
+        }
+    }
 }
