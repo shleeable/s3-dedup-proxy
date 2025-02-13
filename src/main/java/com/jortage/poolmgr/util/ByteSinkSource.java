0de09106621aa5472ea3959c4f721a832d086391
Shlee <github@shl.ee>
2025-02-13 13:49:31 +1030

    Update ByteSinkSource.java

--- src/main/java/com/jortage/poolmgr/util/ByteSinkSource.java
+++ src/main/java/com/jortage/poolmgr/util/ByteSinkSource.java
@@ -5,8 +5,21 @@ import com.google.common.io.ByteSink;
 import com.google.common.io.ByteSource;
 
 public interface ByteSinkSource extends Closeable {
-	ByteSink getSink();
-	ByteSource getSource();
-	@Override
-	void close();
+
+    /**
+     * Returns a ByteSink for writing data.
+     *
+     * @return a ByteSink instance.
+     */
+    ByteSink getSink();
+
+    /**
+     * Returns a ByteSource for reading data.
+     *
+     * @return a ByteSource instance.
+     */
+    ByteSource getSource();
+
+    @Override
+    void close();
 }
